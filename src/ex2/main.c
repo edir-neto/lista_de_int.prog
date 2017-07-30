f15900967c7f58d5ab9d9895adba7f930ad532ac
Edir Neto <edir.paula.neto@gmail.com>
2017-07-30 16:29:15 -0400

    corrigindo programa

--- src/ex2/main.c
+++ src/ex2/main.c
@@ -6,7 +6,7 @@ int main()
     char nome[10][20], sexo[10][1] ;
     int idade[10], indice;
 
-    for(indice = 0; indice < 2; indice++)
+    for(indice = 0; indice < 10; indice++)
     {
         printf("Informe seu primeiro nome:\n");
         scanf("%s", nome[indice]);
@@ -14,11 +14,11 @@ int main()
         scanf("%s", sexo[indice]);
         printf("Informe sua idade:\n");
         scanf("%d", &idade[indice]);
-    }edi
+    }
 
     printf("Pessoas do sexo feminino com idade maior que 21:\n");
 
-    for (indice = 0; indice < 2; indice++)
+    for (indice = 0; indice < 10; indice++)
     {
         if (sexo[indice][0] == 'f' || sexo[indice][0] == 'F')
         {
